6d98756ff52baff73a627ecc4645c4faf0985abe
josea <josealvarado1337@gmail.com>
2020-08-15 19:18:59 -0500

    - Finished with the  Mood Component - Refactored the Counter Component.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -56,15 +56,15 @@ export default function Counter() {
 
   const increment = () => {
     /* STEP 4 */
-    setCount(() => count + 1);
+    setCount(count + 1);
   };
   const decrement = () => {
     /* STEP 5 */
-    setCount(() => count - 1);
+    setCount(count - 1);
   };
   const reset = () => {
     /* STEP 6 */
-    setCount(() =>  initialNumber);
+    setCount(initialNumber);
   };
 
   const style = {
