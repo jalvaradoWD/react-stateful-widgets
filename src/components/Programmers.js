273c461d19c1d7eb14f78d4de64364a8720fff9c
josea <josealvarado1337@gmail.com>
2020-08-15 20:12:03 -0500

    Finished with the Pogrammer component.

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -11,22 +11,24 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from "react";
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
 const listOfAwesome = [
-  { id: '1', name: 'Ada Lovelace' },
-  { id: '2', name: 'Grace Hopper' },
-  { id: '3', name: 'Evelyn Boyd Granville' },
-  { id: '4', name: 'Mary Kenneth Keller' },
-  { id: '5', name: 'Frances Allen' },
-  { id: '6', name: 'Carol Shaw' },
+  { id: "1", name: "Ada Lovelace" },
+  { id: "2", name: "Grace Hopper" },
+  { id: "3", name: "Evelyn Boyd Granville" },
+  { id: "4", name: "Mary Kenneth Keller" },
+  { id: "5", name: "Frances Allen" },
+  { id: "6", name: "Carol Shaw" },
 ];
 
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
+  const [programmerList, setProgrammerList] = useState(listOfAwesome);
+  const [featuredProgrammer, setFeaturedProgrammer] = useState(null);
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -34,36 +36,50 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    const foundProgrammer = programmerList.filter(({id})=>id===featuredProgrammer)[0].name;
+    return foundProgrammer
   };
 
   const style = {
-    fontSize: '1.5em',
-    marginTop: '0.5em',
-    color: 'royalblue', // 🤔
+    fontSize: "1.5em",
+    marginTop: "0.5em",
+    color: featuredProgrammer === null ? "royalblue" : "crimson", // 🤔
   };
 
   return (
-    <div className='widget-programmers container'>
+    <div className="widget-programmers container">
       <h2>Programmers</h2>
-      <div className='programmers'>
+      <div className="programmers">
         {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          listOfAwesome.map(dev =>
+          programmerList.map((dev) => (
             <div key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name}{" "}
+              <button
+                onClick={() => {
+                  /* in here set the featured id to be dev.id */
+                  setFeaturedProgrammer(dev.id);
+                }}
+              >
+                Feature
+              </button>
             </div>
-          )
+          ))
         }
       </div>
       {
         // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
         // Pseudo-code: if the currently featured id is truthy render div 1, otherwise render div 2.
         // Replace the hard-coded false with the correct variable.
-        false
-          ? <div style={style}>🎉 Let&apos;s celebrate {getNameOfFeatured()}! 🥳</div>
-          : <div style={style}>Pick an awesome programmer</div>
+        featuredProgrammer ? (
+          <div style={style}>
+            🎉 Let&apos;s celebrate {getNameOfFeatured()}! 🥳
+          </div>
+        ) : (
+          <div style={style}>Pick an awesome programmer</div>
+        )
       }
     </div>
   );
